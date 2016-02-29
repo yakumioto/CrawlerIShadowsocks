2c3fc49e4f0dbf1c7ca4bc206121cd20ccd833e6
iyannik0215 <iyannik0215@gmail.com>
2016-03-01 01:18:50 +0800

    小改动，加了执行成功提示。

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
+import "log"
+
 func main() {
 	configPath := "./gui-config.json"
 	iConfig := ParsePage(GetPage())
 	WriteProfile(configPath, ModifyProfile(AnalysisProfile(ReadProfile(configPath)), ConvertMap(iConfig)))
+	log.Println("执行成功。")
+	for {
+	}
 }
